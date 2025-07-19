bbb2d1a9254b6a9f096de3201bb77a9c840f9c23
maryam <fatimaisherewithyou@gmail.com>
2025-07-19 11:53:15 +0330

    On Going

---
+++ script.js
@@ -0,0 +1,7 @@
+const button=document.querySelector('button')
+const SideBar= document.querySelector('.Sidebar')
+
+button.onclick = () =>{
+    SideBar.classList.toggle('show')
+    button.classList.toggle('open')
+}
