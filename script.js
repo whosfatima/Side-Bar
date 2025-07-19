d067dae71a158d3f0c3a0bac000ae1150004b0a0
maryam <fatimaisherewithyou@gmail.com>
2025-07-19 12:32:33 +0330

    Done

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const button=document.querySelector('button')
-const SideBar= document.querySelector('.Sidebar')
+const button = document.querySelector("button");
+const SideBar = document.querySelector(".Sidebar");
 
-button.onclick = () =>{
-    SideBar.classList.toggle('show')
-    button.classList.toggle('open')
-}
+button.onclick = () => {
+  SideBar.classList.toggle("show");
+  button.classList.toggle("open");
+};
